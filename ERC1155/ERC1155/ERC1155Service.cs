6afb1c5d5ce1189f6993a169e4a398ba499fe0f1
Jason Benner <jbenner1984@gmail.com>
2021-03-21 19:38:35 -0400

    Add files via upload

---
+++ ERC1155/ERC1155/ERC1155Service.cs
@@ -0,0 +1,214 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Contracts.CQS;
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Contracts;
+using System.Threading;
+using ERC1155.Contracts.ERC1155.ContractDefinition;
+
+namespace ERC1155.Contracts.ERC1155
+{
+    public partial class ERC1155Service
+    {
+        public static Task<TransactionReceipt> DeployContractAndWaitForReceiptAsync(Nethereum.Web3.Web3 web3, ERC1155Deployment eRC1155Deployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            return web3.Eth.GetContractDeploymentHandler<ERC1155Deployment>().SendRequestAndWaitForReceiptAsync(eRC1155Deployment, cancellationTokenSource);
+        }
+
+        public static Task<string> DeployContractAsync(Nethereum.Web3.Web3 web3, ERC1155Deployment eRC1155Deployment)
+        {
+            return web3.Eth.GetContractDeploymentHandler<ERC1155Deployment>().SendRequestAsync(eRC1155Deployment);
+        }
+
+        public static async Task<ERC1155Service> DeployContractAndGetServiceAsync(Nethereum.Web3.Web3 web3, ERC1155Deployment eRC1155Deployment, CancellationTokenSource cancellationTokenSource = null)
+        {
+            var receipt = await DeployContractAndWaitForReceiptAsync(web3, eRC1155Deployment, cancellationTokenSource);
+            return new ERC1155Service(web3, receipt.ContractAddress);
+        }
+
+        protected Nethereum.Web3.Web3 Web3{ get; }
+
+        public ContractHandler ContractHandler { get; }
+
+        public ERC1155Service(Nethereum.Web3.Web3 web3, string contractAddress)
+        {
+            Web3 = web3;
+            ContractHandler = web3.Eth.GetContractHandler(contractAddress);
+        }
+
+        public Task<BigInteger> BalanceOfQueryAsync(BalanceOfFunction balanceOfFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<BalanceOfFunction, BigInteger>(balanceOfFunction, blockParameter);
+        }
+
+        
+        public Task<BigInteger> BalanceOfQueryAsync(string account, BigInteger id, BlockParameter blockParameter = null)
+        {
+            var balanceOfFunction = new BalanceOfFunction();
+                balanceOfFunction.Account = account;
+                balanceOfFunction.Id = id;
+            
+            return ContractHandler.QueryAsync<BalanceOfFunction, BigInteger>(balanceOfFunction, blockParameter);
+        }
+
+        public Task<List<BigInteger>> BalanceOfBatchQueryAsync(BalanceOfBatchFunction balanceOfBatchFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<BalanceOfBatchFunction, List<BigInteger>>(balanceOfBatchFunction, blockParameter);
+        }
+
+        
+        public Task<List<BigInteger>> BalanceOfBatchQueryAsync(List<string> accounts, List<BigInteger> ids, BlockParameter blockParameter = null)
+        {
+            var balanceOfBatchFunction = new BalanceOfBatchFunction();
+                balanceOfBatchFunction.Accounts = accounts;
+                balanceOfBatchFunction.Ids = ids;
+            
+            return ContractHandler.QueryAsync<BalanceOfBatchFunction, List<BigInteger>>(balanceOfBatchFunction, blockParameter);
+        }
+
+        public Task<bool> IsApprovedForAllQueryAsync(IsApprovedForAllFunction isApprovedForAllFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<IsApprovedForAllFunction, bool>(isApprovedForAllFunction, blockParameter);
+        }
+
+        
+        public Task<bool> IsApprovedForAllQueryAsync(string account, string operator, BlockParameter blockParameter = null)
+        {
+            var isApprovedForAllFunction = new IsApprovedForAllFunction();
+                isApprovedForAllFunction.Account = account;
+                isApprovedForAllFunction.Operator = operator;
+            
+            return ContractHandler.QueryAsync<IsApprovedForAllFunction, bool>(isApprovedForAllFunction, blockParameter);
+        }
+
+        public Task<string> SafeBatchTransferFromRequestAsync(SafeBatchTransferFromFunction safeBatchTransferFromFunction)
+        {
+             return ContractHandler.SendRequestAsync(safeBatchTransferFromFunction);
+        }
+
+        public Task<TransactionReceipt> SafeBatchTransferFromRequestAndWaitForReceiptAsync(SafeBatchTransferFromFunction safeBatchTransferFromFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(safeBatchTransferFromFunction, cancellationToken);
+        }
+
+        public Task<string> SafeBatchTransferFromRequestAsync(string from, string to, List<BigInteger> ids, List<BigInteger> amounts, byte[] data)
+        {
+            var safeBatchTransferFromFunction = new SafeBatchTransferFromFunction();
+                safeBatchTransferFromFunction.From = from;
+                safeBatchTransferFromFunction.To = to;
+                safeBatchTransferFromFunction.Ids = ids;
+                safeBatchTransferFromFunction.Amounts = amounts;
+                safeBatchTransferFromFunction.Data = data;
+            
+             return ContractHandler.SendRequestAsync(safeBatchTransferFromFunction);
+        }
+
+        public Task<TransactionReceipt> SafeBatchTransferFromRequestAndWaitForReceiptAsync(string from, string to, List<BigInteger> ids, List<BigInteger> amounts, byte[] data, CancellationTokenSource cancellationToken = null)
+        {
+            var safeBatchTransferFromFunction = new SafeBatchTransferFromFunction();
+                safeBatchTransferFromFunction.From = from;
+                safeBatchTransferFromFunction.To = to;
+                safeBatchTransferFromFunction.Ids = ids;
+                safeBatchTransferFromFunction.Amounts = amounts;
+                safeBatchTransferFromFunction.Data = data;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(safeBatchTransferFromFunction, cancellationToken);
+        }
+
+        public Task<string> SafeTransferFromRequestAsync(SafeTransferFromFunction safeTransferFromFunction)
+        {
+             return ContractHandler.SendRequestAsync(safeTransferFromFunction);
+        }
+
+        public Task<TransactionReceipt> SafeTransferFromRequestAndWaitForReceiptAsync(SafeTransferFromFunction safeTransferFromFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(safeTransferFromFunction, cancellationToken);
+        }
+
+        public Task<string> SafeTransferFromRequestAsync(string from, string to, BigInteger id, BigInteger amount, byte[] data)
+        {
+            var safeTransferFromFunction = new SafeTransferFromFunction();
+                safeTransferFromFunction.From = from;
+                safeTransferFromFunction.To = to;
+                safeTransferFromFunction.Id = id;
+                safeTransferFromFunction.Amount = amount;
+                safeTransferFromFunction.Data = data;
+            
+             return ContractHandler.SendRequestAsync(safeTransferFromFunction);
+        }
+
+        public Task<TransactionReceipt> SafeTransferFromRequestAndWaitForReceiptAsync(string from, string to, BigInteger id, BigInteger amount, byte[] data, CancellationTokenSource cancellationToken = null)
+        {
+            var safeTransferFromFunction = new SafeTransferFromFunction();
+                safeTransferFromFunction.From = from;
+                safeTransferFromFunction.To = to;
+                safeTransferFromFunction.Id = id;
+                safeTransferFromFunction.Amount = amount;
+                safeTransferFromFunction.Data = data;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(safeTransferFromFunction, cancellationToken);
+        }
+
+        public Task<string> SetApprovalForAllRequestAsync(SetApprovalForAllFunction setApprovalForAllFunction)
+        {
+             return ContractHandler.SendRequestAsync(setApprovalForAllFunction);
+        }
+
+        public Task<TransactionReceipt> SetApprovalForAllRequestAndWaitForReceiptAsync(SetApprovalForAllFunction setApprovalForAllFunction, CancellationTokenSource cancellationToken = null)
+        {
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(setApprovalForAllFunction, cancellationToken);
+        }
+
+        public Task<string> SetApprovalForAllRequestAsync(string operator, bool approved)
+        {
+            var setApprovalForAllFunction = new SetApprovalForAllFunction();
+                setApprovalForAllFunction.Operator = operator;
+                setApprovalForAllFunction.Approved = approved;
+            
+             return ContractHandler.SendRequestAsync(setApprovalForAllFunction);
+        }
+
+        public Task<TransactionReceipt> SetApprovalForAllRequestAndWaitForReceiptAsync(string operator, bool approved, CancellationTokenSource cancellationToken = null)
+        {
+            var setApprovalForAllFunction = new SetApprovalForAllFunction();
+                setApprovalForAllFunction.Operator = operator;
+                setApprovalForAllFunction.Approved = approved;
+            
+             return ContractHandler.SendRequestAndWaitForReceiptAsync(setApprovalForAllFunction, cancellationToken);
+        }
+
+        public Task<bool> SupportsInterfaceQueryAsync(SupportsInterfaceFunction supportsInterfaceFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<SupportsInterfaceFunction, bool>(supportsInterfaceFunction, blockParameter);
+        }
+
+        
+        public Task<bool> SupportsInterfaceQueryAsync(byte[] interfaceId, BlockParameter blockParameter = null)
+        {
+            var supportsInterfaceFunction = new SupportsInterfaceFunction();
+                supportsInterfaceFunction.InterfaceId = interfaceId;
+            
+            return ContractHandler.QueryAsync<SupportsInterfaceFunction, bool>(supportsInterfaceFunction, blockParameter);
+        }
+
+        public Task<string> UriQueryAsync(UriFunction uriFunction, BlockParameter blockParameter = null)
+        {
+            return ContractHandler.QueryAsync<UriFunction, string>(uriFunction, blockParameter);
+        }
+
+        
+        public Task<string> UriQueryAsync(BigInteger returnValue1, BlockParameter blockParameter = null)
+        {
+            var uriFunction = new UriFunction();
+                uriFunction.ReturnValue1 = returnValue1;
+            
+            return ContractHandler.QueryAsync<UriFunction, string>(uriFunction, blockParameter);
+        }
+    }
+}
